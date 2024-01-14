9f508d504f03c84a804e97a6456a1f5130ad6f9f
Mark Bosco <mab688@pitt.edu>
2024-01-14 12:26:33 -0500

    Added first page routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-//import App from './App';
-import SearchPage from './pages/Search/SearchPage'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import HomePage from "./pages/Home/HomePage"
+import SearchPage from "./pages/Search/SearchPage"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/search",
+    element: <SearchPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SearchPage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -5,8 +5,7 @@ function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/search">Search</Link>
     </div>
   );
 }
